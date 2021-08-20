6d6664f0a27d2138c89e07322bd25f89dbfc4710
ignaciokh <nachokholmanian@hotmail.com>
2021-08-19 21:19:23 -0300

    Tercer Commit

--- js/login.js
+++ js/login.js
@@ -4,3 +4,16 @@
 document.addEventListener("DOMContentLoaded", function(e){
 
 });
+
+
+function enviar(){
+    let usuario = document.getElementById("usuario").value;
+    let contraseña = document.getElementById("contraseña").value
+
+    if(usuario === "" || contraseña === ""){
+        alert("Debe cargar todos los datos para poder continuar");
+        document.getElementById('formulario-login').reset();
+    }else{
+        window.location.href = "home.html";
+    }
+}
