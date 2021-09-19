da7fc9ad12b648312a8416c5e7164c2cac413f8f
ignaciokh <nachokholmanian@hotmail.com>
2021-09-18 22:19:16 -0300

    Commite 11

--- js/init.js
+++ js/init.js
@@ -18,15 +18,16 @@ function validacion() {
       let contenedor = document.getElementsByClassName("container d-flex flex-column flex-md-row justify-content-between");
       let infoUsuario = document.createElement("p");
       infoUsuario.innerHTML = `
-      <div class = "fixed-top ml-3">
+      <div class = "align-self-start">
       <div class="chip">
       <img src="img/img_avatar.png" alt="Person" width="96" height="96">
       ${usuario}
       </div>
       </div>
-    `;
+      `;
 
-      contenedor[0].appendChild(infoUsuario);
+      contenedor[0].prepend(infoUsuario);
+      //infoUsuario.className = "fixed-top ml-3";
     }
   }
 }
