98679947ce9c924583993c09c9d5560aedc4f19e
“Ignacio <nachokholmanian@hotmail.com>
2024-06-09 22:39:08 -0300

    Removing username and password request

--- js/login.js
+++ js/login.js
@@ -6,13 +6,8 @@ document.getElementById("button-Login").addEventListener("click", function(e){
     let usuario = document.getElementById("usuario").value;
     let contrasena = document.getElementById("contrasena").value
 
-    if(usuario !== "Ignacio" || contrasena !== "NewPass"){
-        alert("Usuario o contraseña incorrecto");
-        document.getElementById('formulario-login').reset();
-    }else{
-        localStorage.setItem("usuario", usuario); //setItem almacena el dato en la posición "usuario"
-        localStorage.setItem("contrasena", contrasena);
-        window.location.href = "index.html";
-    }
+    localStorage.setItem("usuario", usuario); //setItem almacena el dato en la posición "usuario"
+    localStorage.setItem("contrasena", contrasena);
+    window.location.href = "index.html";
 
 });
